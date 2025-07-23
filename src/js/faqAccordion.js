daf419114d9a77e8d9dc934e875b6e399da2b2e6
Konstantin <bashuk.kostya@gmail.com>
2025-07-23 18:12:47 +0300

    faq completed

--- src/js/faqAccordion.js
+++ src/js/faqAccordion.js
@@ -2,16 +2,17 @@ const accordions = document.querySelectorAll('[data-accordion]');
 
 accordions.forEach(btn => {
   btn.addEventListener('click', function () {
+    const panel = this.nextElementSibling;
+
     const isOpen = this.dataset.active === 'true';
     this.dataset.active = (!isOpen).toString();
 
-    const panel = this.nextElementSibling;
     if (isOpen) {
       panel.style.maxHeight = null;
       delete panel.dataset.active;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + 'px';
       panel.dataset.active = 'true';
+      panel.style.maxHeight = panel.scrollHeight + 'px';
     }
   });
 });
